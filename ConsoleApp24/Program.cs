7252f4402a0fb49b90cc0a04fe61ba2e3a1fa902
Alex Chaplygin <chapalexander@gmail.com>
2023-03-18 01:11:19 +0300

    Add project files.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,143 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+class ListNode
+{
+    public ListNode Prev;
+    public ListNode Next;
+    public ListNode Rand; // произвольный элемент внутри списка
+    public string Data;
+
+    public ListNode(ListNode rand, string data) //for tests
+    {
+        Rand = rand;
+        Data = data;
+    }
+
+    public ListNode() : this(null, null) { }
+
+    public void ConnectToPrev(ListNode other)
+    {
+        this.Prev = other;
+        other.Next = this;
+    }
+
+    public override string ToString()
+    {
+        return $"Prev: {this.Prev}, Next: {this.Next}, Rand: {this.Rand}, Data: {this.Data}";
+    }
+}
+
+class ListRand
+{
+    public ListNode Head;
+    public ListNode Tail;
+    public int Count;
+
+    public ListRand(ListNode head, ListNode tail, int count) //for tests
+    {
+        Head = head;
+        Tail = tail;
+        Count = count;
+    }
+
+    public override string ToString()
+    {
+        var numberByNode = GetOrderedDictionary();
+        string listNodeInformation = ConvertToString(numberByNode);
+        return listNodeInformation;
+    }
+    public void Serialize(FileStream s)
+    {
+        var numberByNode = GetOrderedDictionary();
+        string listNodeInformation = ConvertToString(numberByNode);
+        byte[] encodedInformation = Encoding.ASCII.GetBytes(listNodeInformation.ToString());
+        s.Write(encodedInformation);
+    }
+
+    public void Deserialize(FileStream s)
+    {
+        var nodes = CreateNodes();
+        string decodedLine = DecodeBytes(s);
+        AnalyzeLine(decodedLine, nodes);
+    }
+
+    public Dictionary<ListNode, int> GetOrderedDictionary()
+    {
+        var dictionary = new Dictionary<ListNode, int>();
+        int index = 0;
+        foreach (var item in Nodes())
+        {
+            dictionary[item] = index++;
+        }
+        return dictionary;
+    }
+
+    public string ConvertToString(Dictionary<ListNode,int> dictionary)
+    {
+        var output = new StringBuilder();
+        foreach (var node in Nodes())
+        {
+            var rand = node.Rand;
+            int number = rand == null ? -1 : dictionary[rand];
+            string nodeInformation = $"{number},'{node.Data}'";
+            output.Append("(" + nodeInformation + ")\n");
+        }
+        output.Length = output.Length - 1;
+        return output.ToString();
+    }
+
+    public ListNode[] CreateNodes()
+    {
+        var nodes = new ListNode[Count]; 
+        nodes[0] = Head;
+        nodes[Count - 1] = Tail;
+
+        var prevNode = Head;
+        for (int i = 1; i < Count - 1; i++)
+        {
+            var current = new ListNode();
+            nodes[i] = current;
+            current.ConnectToPrev(prevNode);
+            prevNode = current;
+        }
+        Tail.ConnectToPrev(prevNode);
+        return nodes;
+    }
+
+    public string DecodeBytes(FileStream s)
+    {
+        byte[] encodedInformation = File.ReadAllBytes(s.Name);
+        string decoded = Encoding.ASCII.GetString(encodedInformation);
+        return decoded;
+    }
+
+    public void AnalyzeLine(string line, ListNode[] nodes)
+    {
+        string pattern = @"(?<=((?<Rand>-*\d),'))(?<Data>.*)(?=('\)))";
+        var mathes = Regex.Matches(line, pattern, RegexOptions.ExplicitCapture);
+        int index = 0;
+        foreach (Match matchItem in mathes)
+        {
+            var randValue = matchItem.Groups["Rand"].Value;
+            int randNumber = Int32.Parse(randValue);
+            string data = matchItem.Groups["Data"].Value.Replace(@"\n", "\n");
+
+            var current = nodes[index++];
+            current.Rand = randNumber != -1 ? nodes[randNumber] : null;
+            current.Data = data;
+        }
+    }
+
+    public IEnumerable<ListNode> Nodes()
+    {
+        var value = Head;
+        while (value != null)
+        {
+            yield return value;
+            value = value.Next;
+        }
+    }
+}
+
+
---
+++ ConsoleApp24/Tests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using NUnit;
+
+namespace TestProject
+{
+    public class Tests
+    {
+
+        [TestFixture]
+        public class CorrectReading
+        {
+            //[Test]
+            //public void Serialization_1()
+            //{
+            //    string input = @"[-1,0][1,1][4,2][0,3][-1,4]";
+            //    Assertion(input);
+            //}
+
+            [Test]
+            //public void Serialization_2()///сделать чрез testcase+case sob[]aka
+            //{
+            //    string input = @"[-1,a][-1,b][-1,c][-1,d]";
+            //    Assertion(input);
+            //}
+
+            //public void Assertion(string input)
+            //{
+            //    var list = input.ConvertToListRand();
+            //    FileStream stream = new FileStream("file.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //    stream.Position = 0;
+            //    var numberedDictionary = list.GetOrderedDictionary();
+            //    var output = list.ConvertToString(numberedDictionary);
+
+            //    Assert.That(output, Is.EqualTo(input));
+            //}
+
+            public void SerializeAndDeserialize()
+            {
+                ListNode head = new ListNode(null, "head");
+                ListNode tail = new ListNode(null, "tail");
+
+                ListNode secondNode = new ListNode(null, "second");
+                secondNode.Rand = secondNode;
+                secondNode.ConnectToPrev(head);
+
+                ListNode thirdNode = new ListNode(tail, "third");
+                thirdNode.ConnectToPrev(secondNode);
+
+                ListNode fourthNode = new ListNode(head, "4th");
+                fourthNode.ConnectToPrev(thirdNode);
+
+                tail.ConnectToPrev(fourthNode);
+
+                var list = new ListRand(head, tail, 5);
+                using (var stream = new FileStream("4", FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                {
+                    list.Serialize(stream);
+                }
+                using (var stream = new FileStream("4", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    list.Deserialize(stream);
+                }
+                Assert.That(("(-1,'head')\n(1,'second')\n(4,'third')\n(0,'4th')\n(-1,'tail')"), Is.EqualTo(list.ToString()));
+                
+            }
+        }
+    }
+
+}
